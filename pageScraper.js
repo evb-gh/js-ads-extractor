c7ac744ee13d22675206ab5f28408e3ca0be6ed1
evb <enbotnaru@gmail.com>
2022-08-13 11:39:11 -0700

    fix code for pagination

--- pageScraper.js
+++ pageScraper.js
@@ -1,78 +1,96 @@
 const scraperObject = {
 
-    url: 'https://www.indeed.com/jobs?q=teacher&l=Nevada',
+    url: 'https://www.indeed.com/jobs?q=Driver&l=Texas',
+    page: 0,
 
     async scraper(browser) {
 
         let page = await browser.newPage();
+        console.log(`Navigating to ${this.url}...`);
+        await page.goto(this.url);
         let scrapedData = [];
 
-        console.log(`Navigating to ${this.url}...`);
+        async function scrapeCurrentPage() {
 
-        await page.goto(this.url);
-        await page.waitForSelector('.mosaic-zone');
+            await page.waitForSelector('.mosaic-zone');
 
-        // Get the link to all the required books
-        let urls = await page.evaluate(() => {
+            // Get the link to all the required books
+            let urls = await page.evaluate(() => {
 
-            try {
-                return [...document.querySelectorAll('h2 > a')].map(elem => elem.href)
-            } catch (e) {
-                console.error(e);
+                try {
+                    return [...document.querySelectorAll('h2 > a')].map(elem => elem.href)
+                } catch (e) {
+                    console.error(e);
+                }
+            });
+
+            let pagePromise = (link) => new Promise(async (resolve, reject) => {
+
+                let dataObj = {};
+                let newPage = await browser.newPage();
+
+                const jobTitleSelector = 'h1.jobsearch-JobInfoHeader-title'
+                const jobLocationSelector = 'div.jobsearch-CompanyInfoContainer > div > div > div > div:nth-child(2)'
+                // const companyNameSelector = 'div.jobsearch-CompanyInfoContainer a'
+                const companyNameSelector = 'div.jobsearch-CompanyInfoContainer > div > div > div > div.jobsearch-InlineCompanyRating > div:nth-child(2) > div'
+
+                await newPage.goto(link);
+
+                dataObj['jobTitle'] = await newPage.$eval(jobTitleSelector, text => text.textContent);
+                dataObj['jobLocation'] = await newPage.$eval(jobLocationSelector, text => text.innerText);
+                dataObj['companyName'] = await newPage.$eval(companyNameSelector, text => text.innerText);
+
+                let jobKey = await newPage.$eval('script#mosaic-data', text => text.textContent.match(/(?<="jobKey":")[\w\d]*/)[0]);
+                dataObj['jobUrl'] = `https://www.indeed.com/viewjob?jk=${jobKey}`
+                dataObj['jobID'] = jobKey
+
+                resolve(dataObj);
+
+                await newPage.close();
+            });
+
+            for (link in urls) {
+
+                let currentPageData = await pagePromise(urls[link]);
+
+                scrapedData.push(currentPageData);
+                // console.log(currentPageData);
             }
-        });
 
-        let pagePromise = (link) => new Promise(async (resolve, reject) => {
+            // const paginationSelector = 'ul.pagination-list > li'
+            await page.waitForSelector('nav[aria-label="pagination"]');
 
-            let dataObj = {};
-            let newPage = await browser.newPage();
+            const exists = await page.$eval('ul.pagination-list', () => true).catch(() => false)
 
-            const jobTitleSelector = 'h1.jobsearch-JobInfoHeader-title'
-            const jobLocationSelector = 'div.jobsearch-CompanyInfoContainer > div > div > div > div:nth-child(2)'
-            // const companyNameSelector = 'div.jobsearch-CompanyInfoContainer a'
-            const companyNameSelector = 'div.jobsearch-CompanyInfoContainer > div > div > div > div.jobsearch-InlineCompanyRating > div:nth-child(2) > div'
+            console.log(exists);
 
-            await newPage.goto(link);
+            let nextPageExists;
 
-            dataObj['jobTitle'] = await newPage.$eval(jobTitleSelector, text => text.textContent);
-            dataObj['jobLocation'] = await newPage.$eval(jobLocationSelector, text => text.innerText);
-            dataObj['companyName'] = await newPage.$eval(companyNameSelector, text => text.innerText);
+            if (exists) {
+                nextPageExists = (await page.$$eval('ul.pagination-list > li', list => list[list.length - 1].textContent)) == '';
+            } else {
+                nextPageExists = (await page.$$eval('nav[aria-label="pagination"] > div', list => list[list.length - 1].textContent)) == '';
+            }
 
-            let jobKey = await newPage.$eval('script#mosaic-data', text => text.textContent.match(/(?<="jobKey":")[\w\d]*/)[0]);
-            dataObj['jobUrl'] = `https://www.indeed.com/viewjob?jk=${jobKey}`
-            dataObj['jobID'] = jobKey
+            console.log(nextPageExists)
 
-            resolve(dataObj);
+            if (nextPageExists) {
+                scraperObject.page += 10
+                console.log(scraperObject.page)
+                console.log(`${scraperObject.url}&start=${scraperObject.page}`)
+                let pageUrl = `${scraperObject.url}&start=${scraperObject.page}`;
+                await page.goto(pageUrl);
 
-            await newPage.close();
-        });
+                return scrapeCurrentPage(); // Call this function recursively
+            }
 
-        for (link in urls) {
+            // await page.close();
 
-            let currentPageData = await pagePromise(urls[link]);
-
-            scrapedData.push(currentPageData);
-            // console.log(currentPageData);
+            return scrapedData;
         }
-
-        let nextButtonExist = false;
-
-        try {
-            const nextButton = await page.$eval('ul.pagination-list > a', a => a.textContent);
-            nextButtonExist = true;
-        }
-        catch (err) {
-            nextButtonExist = false;
-        }
-
-        if (nextButtonExist) {
-            await page.click('.next > a');
-            return scrapeCurrentPage(); // Call this function recursively
-        }
-
-        // await page.close();
-
-        return scrapedData;
+        let data = await scrapeCurrentPage();
+        console.log(data);
+        return data;
     }
     // console.log(urls, typeof (urls));
 }
